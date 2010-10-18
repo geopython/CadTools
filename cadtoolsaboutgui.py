4ad3452c828a5f5449d6b84a11b426fdcb124fa4
edi.gonzales <edi.gonzales@5cb8516b-641c-f86c-07e9-861e6a0574f2>
2010-10-18 19:18:56 +0000

    qgis_cadtools: help for circular arcs added.
    git-svn-id: http://cataisrepository.googlecode.com/svn/trunk/qgis/plugins/cadtools@28 5cb8516b-641c-f86c-07e9-861e6a0574f2

--- cadtoolsaboutgui.py
+++ cadtoolsaboutgui.py
@@ -14,8 +14,7 @@ class CadToolsAboutGui(QDialog, QObject, Ui_CadToolsAbout):
         self.setupUi(self)
         QObject.connect(self.btnWeb, SIGNAL("clicked()"), self.openWeb)
         QObject.connect(self.btnHelp, SIGNAL("clicked()"), self.openHelp)
-        #self.CadToolsLogo.setPixmap(QPixmap(":/icons/default/ftools_logo.png"))
-        self.lblVersion.setText("CadTools 0.4.4")
+        self.lblVersion.setText("CadTools 0.4.9")
         self.txtAbout.setText(self.getText())    
     
     def openWeb(self):
@@ -32,6 +31,7 @@ There is some code adopted from the Numerical Vertex Edit plugin (Cédric Möri), 
 
 LICENSING INFORMATION:
 CadTools is copyright (C) 2009-2010  Stefan Ziegler
+edi.gonzales@gmail.com
 
 Licensed under the terms of GNU GPL 2
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
